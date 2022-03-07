cb6ada700fa3a909b4553043297a74acdacd3b4c
ahmedfarouk-develop <ahmedfarouk6066@gmail.com>
2022-03-07 21:27:08 +0100

    Anew Class practice Methods.java from java language

---
+++ src/main/java/priceCalculator/AppRanner.java
@@ -0,0 +1,21 @@
+package priceCalculator;
+
+import java.util.Scanner;
+
+public class AppRanner {
+    public static void main (String[]arg){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Start Call = ");
+        String startCall = input.next();
+        System.out.println("end Call = ");
+        String endCall  = input.next();
+
+        CostCallCalculator cost = new CostCallCalculator(startCall,endCall);
+        cost.calculateCost();
+        System.out.println("Cost of Call == "+ cost.calculateCost());
+    }
+
+
+}
